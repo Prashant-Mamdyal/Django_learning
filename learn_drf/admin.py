ebc61d461b2476e5996134022ac4f30f4cc3dca7
Prashant-Mamdyal <prashant.mamdyal68@gmail.com>
2024-06-24 18:49:22 +0530

    created basic view functions and returned data using Jsonresponse

---
+++ learn_drf/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from learn_drf.models import Movie
+
+# Register your models here.
+admin.site.register(Movie)
---
+++ learn_drf/views.py
@@ -0,0 +1,19 @@
+from learn_drf.models import Movie
+from django.http import JsonResponse
+
+# Creating basic views and returning data using Jsonresponse.
+def movie_list(request):
+    movie = Movie.objects.all()             #accessing the whole movie list
+    data = {
+        'movie':list(movie.values())        #here converting complex queryset data into python dictionary using values() method 
+    }                                       #and then converting python dictionary into json format using Jsonresponse                                              
+    return JsonResponse(data)
+
+def movie_details(request, pk):             #accesssing perticular movie details using id
+    movie = Movie.objects.get(id = pk)
+    data = {
+        'name': movie.name,
+        'description': movie.description,
+        'status': movie.active
+    }
+    return JsonResponse(data)
