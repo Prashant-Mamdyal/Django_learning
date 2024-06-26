57727663a321eb81f9b7f57abd58bd7f9c508115
Prashant-Mamdyal <prashant.mamdyal68@gmail.com>
2024-06-26 18:17:51 +0530

    implemented different methods like GET, POST, PUT, DELETE

--- learn_drf/api/serializer.py
+++ learn_drf/api/serializer.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
+from learn_drf.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only = True)
     name = serializers.CharField(max_length=20)
     description = serializers.CharField()
     active = serializers.BooleanField()
+
+    # create method is for adding a new data to table. To implement a POST method we use a create method. 
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+    
+    # update method is for updating a perticualar data/record in table. To implement a PUT method we use update method.
+    # we are updating old values with new values.
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
--- learn_drf/api/views.py
+++ learn_drf/api/views.py
@@ -5,14 +5,52 @@ from learn_drf.models import Movie
 from learn_drf.api.serializer import MovieSerializer
 
 
-@api_view()                                         #imported decorator to know which http method we are working on
-def movie_list(request):
-    movie = Movie.objects.all()
-    serializer = MovieSerializer(movie, many=True)      #remember to add 'many=true' if you returning data in list format
-    return Response(serializer.data)
+# @api_view()                                         #imported decorator to know which http method we are working on
+# def movie_list(request):
+#     movie = Movie.objects.all()
+#     serializer = MovieSerializer(movie, many=True)      #remember to add 'many=true' if you returning data in list format
+#     return Response(serializer.data)
 
-@api_view()
+# @api_view()
+# def movie_details(request, pk):
+#     movie = Movie.objects.get(id = pk)
+#     serializer = MovieSerializer(movie)
+#     return Response(serializer.data)
+
+#Implemented different methods like GET, POST, PUT, DELETE.
+@api_view(['GET', 'POST'])                              
+def movie_list(request):
+    if request.method == 'GET':
+        movie = Movie.objects.all()
+        serialier = MovieSerializer(movie, many=True)
+        return Response(serialier.data)
+    
+    if request.method == 'POST':
+        serialier = MovieSerializer(data=request.data)
+        if serialier.is_valid():
+            serialier.save()
+            return Response(serialier.data)
+        else:
+            return Response(serialier.errors)
+
+
+@api_view(['GET','PUT','DELETE'])
 def movie_details(request, pk):
-    movie = Movie.objects.get(id = pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movie = Movie.objects.get(id=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        movie = Movie.objects.get(id=pk)
+        serializer = MovieSerializer(movie, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(id = pk)
+        movie.delete()
+        return Response("Movie is deleted successfully...")
